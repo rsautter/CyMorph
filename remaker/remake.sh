1764f4c810adbf410a9e9c31bf2d0231e8754de9
Rubens <rubens.sautter@gmail.com>
2017-06-19 10:30:50 -0300

    Delete remake.sh

--- remaker/remake.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-awk -F '\,' '{if (NR!=1){print $1}}' result.csv | sort -m > l1.txt
-awk -F '\,' '{if (NR!=1){print $1}}' FilteredGalaxyZoo40.csv | sort -m > l2.txt
-awk '{ h[$0] = ! h[$0] } END { for (k in h) if (h[k]) print k }' l1.txt l2.txt > l3.txt
-python remake.py l3.txt FilteredGalaxyZoo40.csv
-
-
