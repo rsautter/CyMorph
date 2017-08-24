c2c3aee5b29f7a309af34b3fb324e3d9df019767
Rubens <rubens.sautter@gmail.com>
2017-08-24 20:37:58 -0300

    Add files via upload

--- compile.py
+++ compile.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 import numpy
-
+import os
 
 print("Numpy Location:"+numpy.get_include())
 setup(ext_modules=cythonize(["*.pyx"], annotate=True,
@@ -21,3 +21,24 @@ setup(ext_modules=cythonize(["*.pyx"], annotate=True,
 ),
 include_dirs=[numpy.get_include()]
 )
+print("Compiling maskMaker")
+os.chdir("maskMaker/")
+setup(ext_modules=cythonize(["*.pyx"], annotate=True,
+		compiler_directives={
+			'optimize.use_switch': True,
+			'initializedcheck':False,
+			'overflowcheck':False,
+			'optimize.unpack_method_calls':True,
+			'boundscheck':False,
+			'profile': False,
+			'infer_types':True,
+			'cdivision_warnings': False,
+			'cdivision': True,
+			'wraparound': False,
+			'boundscheck': False},
+                include_path=[numpy.get_include()]
+),
+include_dirs=[numpy.get_include()]
+)
+os.chdir("../")
+print("All done!")
