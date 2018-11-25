1ebc8b8fe9031acb22c62ca0bacb69abbd9246ad
Rubens <rubens.sautter@gmail.com>
2018-11-25 15:22:59 -0200

    Add files via upload
    Added the possibility of creating an empty ellipse object (be careful when using it)

--- ellipse.pyx
+++ ellipse.pyx
@@ -12,26 +12,44 @@ cdef class ellipse:
     cdef public float angle, petrosianMag, sky, maxRad, minRad, fwhm
     cdef public float posx, posy
 
-    def __cinit__(self, dic, data,calibratedData,float sky):
-        self.rp = float(galaxyIO.getFirstValue(dic, data, 'PETRO_RADIUS'))
-        self.id = float(galaxyIO.getFirstValue(dic, data, 'NUMBER'))
-        self.posx = float(galaxyIO.getFirstValue(dic, data, 'X_IMAGE'))
-        self.posy = float(galaxyIO.getFirstValue(dic, data, 'Y_IMAGE'))
-        cdef float angulo = radians(float(galaxyIO.getFirstValue(dic, data, 'THETA_IMAGE')))
-        cdef float scaleA = float(galaxyIO.getFirstValue(dic, data, 'A_IMAGE'))
-        cdef float scaleB = float(galaxyIO.getFirstValue(dic, data, 'B_IMAGE'))
-        self.petrosianMag = float(galaxyIO.getFirstValue(dic, data, 'FLUX_PETRO'))
-        self.fwhm = float(galaxyIO.getFirstValue(dic, data, 'FWHM_IMAGE'))
-        if(sky == -1.0):
-            self.sky = float(galaxyIO.getFirstValue(dic, data, 'BACKGROUND'))
+    # Warning! - this builds an empty object
+    cdef void setEmpty(self):
+        print("Warning! - Empty Ellipse Object")
+        self.rp = 0.0
+        self.id = 0.0
+        self.posx = 0.0
+        self.posy = 0.0
+        self.fwhm = 0.0
+        self.petrosianMag = 0.0
+        self.sky = 0.0
+        self.angle = 0.0
+        self.maxRad = 0.0
+        self.minRad = 0.0
+
+    def __cinit__(self, dic=None, data=None,calibratedData=None,float sky=-1.0):
+        cdef float angulo,scaleA,scaleB
+        if(dic is None or data is None or calibratedData is None):
+            self.setEmpty()
         else:
-            self.sky = sky 
-        if (calibratedData):
-            self.petrosianMag = pow(10.0,-0.4*self.petrosianMag)
-        #self.pos = (x,y)
-        self.angle = angulo
-        self.maxRad = scaleA*self.rp
-        self.minRad = scaleB*self.rp
+            self.rp = float(galaxyIO.getFirstValue(dic, data, 'PETRO_RADIUS'))
+            self.id = float(galaxyIO.getFirstValue(dic, data, 'NUMBER'))
+            self.posx = float(galaxyIO.getFirstValue(dic, data, 'X_IMAGE'))
+            self.posy = float(galaxyIO.getFirstValue(dic, data, 'Y_IMAGE'))
+            angulo = radians(float(galaxyIO.getFirstValue(dic, data, 'THETA_IMAGE')))
+            scaleA = float(galaxyIO.getFirstValue(dic, data, 'A_IMAGE'))
+            scaleB = float(galaxyIO.getFirstValue(dic, data, 'B_IMAGE'))
+            self.petrosianMag = float(galaxyIO.getFirstValue(dic, data, 'FLUX_PETRO'))
+            self.fwhm = float(galaxyIO.getFirstValue(dic, data, 'FWHM_IMAGE'))
+            if(sky == -1.0):
+                self.sky = float(galaxyIO.getFirstValue(dic, data, 'BACKGROUND'))
+            else:
+                self.sky = sky 
+            if (calibratedData):
+                self.petrosianMag = pow(10.0,-0.4*self.petrosianMag)
+            #self.pos = (x,y)
+            self.angle = angulo
+            self.maxRad = scaleA*self.rp
+            self.minRad = scaleB*self.rp
  
     ##Encontra a escala da elipse que corta x, y
     @cython.boundscheck(False)
